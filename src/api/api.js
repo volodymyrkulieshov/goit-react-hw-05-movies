54e038b2078cb308f94883cd0db28f7264bc78c1
Volodymyr <vovkul@gmail.com>
2024-01-12 21:26:35 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,11 +4,11 @@ const BASEURL = 'https://api.themoviedb.org/3';
 
 const APIKEY = '55a48febb45b5475689763bd95744d57';
 
-export const getAllMovies = async () => {
+export async function getAllMovies() {
   const url = `${BASEURL}/trending/movie/day?api_key=${APIKEY}`;
   const { data } = await axios.get(url);
   return data.results;
-};
+}
 
 export async function getMovieDetails(movieId) {
   const url = `${BASEURL}/movie/${movieId}?api_key=${APIKEY}`;
